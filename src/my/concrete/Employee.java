9e06b7ba15c238908ee10c842a571d29b4bfd91b
owner <owner@LATITUDE-E6500.attlocal.net>
2014-09-01 13:49:03 -0500

    Created employee & hourlyEmployee class for my.concrete package

--- src/my/concrete/Employee.java
+++ src/my/concrete/Employee.java
@@ -11,5 +11,52 @@ package my.concrete;
  * @author owner
  */
 public class Employee {
+    private String name;
+    private int employeeID;
+    private char gender;
+    private int age;
+    
+    //Constructor
+    public Employee(String name, int employeeID, char gender, int age) {
+        this.name = name;
+        this.employeeID = employeeID;
+        this.gender = gender;
+        this.age = age;
+    }
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
     
 }
--- src/my/concrete/HourlyEmployee.java
+++ src/my/concrete/HourlyEmployee.java
@@ -6,10 +6,42 @@
 
 package my.concrete;
 
-/**
- *
- * @author owner
- */
-public class HourlyEmployee {
+public class HourlyEmployee extends Employee {
+    private double payRate;
+    private double hoursWorked;
+    private double employeePay;
+    
+    //Constructor that inherits superclass's constructor and sets the hourly employee
+    //payrate and number of hours worked
+    /**
+     * 
+     * @param name
+     * @param employeeID
+     * @param gender
+     * @param age
+     * @param payRate
+     * @param hoursWorked 
+     */
+    public HourlyEmployee(String name, int employeeID, char gender, int age, double payRate, double hoursWorked) {
+        super(name, employeeID, gender, age);
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+    
+    //returns the amount an employee made based off their payrate and number of hours worked
+    public double getEmployeePay() {
+        employeePay = (payRate * hoursWorked);
+        
+        return employeePay;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+    
     
 }
