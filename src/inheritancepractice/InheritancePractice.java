7fff93a2dc0387009f2fec949f0889cb308852ed
owner <owner@LATITUDE-E6500.attlocal.net>
2014-09-01 10:45:39 -0500

    First Save

---
+++ src/inheritancepractice/InheritancePractice.java
@@ -0,0 +1,17 @@
+
+package inheritancepractice;
+
+/**
+ *
+ * @Jessica Kramer
+ */
+public class InheritancePractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
