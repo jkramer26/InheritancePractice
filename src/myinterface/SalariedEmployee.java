d68cd56feefd76d6f4a8dcea2854bd2836965775
owner <owner@192.168.0.16>
2014-09-02 02:40:42 -0500

    finished coding last two classes in myinterface package

--- src/myinterface/SalariedEmployee.java
+++ src/myinterface/SalariedEmployee.java
@@ -10,6 +10,70 @@ package myinterface;
  *
  * @author owner
  */
-public class SalariedEmployee {
+public class SalariedEmployee implements Employee {
     
+   private String name;
+   private int employeeID;
+   private char gender;
+   private int age;
+   private double annualSalary;
+   
+   public SalariedEmployee(String name, int employeeID, char gender, int age, double annualSalary) {
+        this.name = name;
+        this.employeeID = employeeID;
+        this.gender = gender;
+        this.age = age;
+        this.annualSalary = annualSalary;
+    }
+   
+   public double getAnnualSalary() {
+        return annualSalary;
+    }
+   
+   //overiding methods from employee interface
+    @Override
+    public String getName() {
+        return name;
+    }
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    @Override
+    public int getEmployeeID() {
+        return employeeID;
+    }
+    @Override
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+    
+    
+    @Override
+    public char getGender() {
+        return gender;
+    }
+    @Override
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    
+    
+    @Override
+    public int getAge() {
+        return age;
+    }
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public double getEmployeePay() {
+        return annualSalary;
+    }
+   
 }
--- src/myinterface/SalaryPlusCommissionEmployee.java
+++ src/myinterface/SalaryPlusCommissionEmployee.java
@@ -1,15 +1,92 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Subclass that extends SalariedEmployee and implements Employee interface
  */
 
 package myinterface;
 
-/**
- *
- * @author owner
- */
-public class SalaryPlusCommissionEmployee {
+
+public class SalaryPlusCommissionEmployee extends SalariedEmployee implements Employee {
+    
+   private String name;
+   private int employeeID;
+   private char gender;
+   private int age;
+   private double annualSalary;
+   private double commissionRate;
+   private double totalSales;
+   private double commissionPay;
+   private double employeePay;
+    
+    //constructor
+    public SalaryPlusCommissionEmployee(String name, int employeeID, char gender, int age, double annualSalary, 
+            double commissionRate, double totalSales) {
+        super(name, employeeID, gender, age, annualSalary);
+        this.commissionRate = commissionRate;
+        this.totalSales = totalSales;
+    }
+    
+    public double getCommissionRate() {
+        return commissionRate;
+    }
+
+    public double getTotalSales() {
+        return totalSales;
+    }
+    
+    public double getCommssionPay() {
+        //calculate the amount of commission pay the employee earned
+        commissionPay = (commissionRate * totalSales);
+        
+        return commissionPay;
+    }
+    
+    
+    //overiding methods from employee interface
+    @Override
+    public String getName() {
+        return name;
+    }
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    @Override
+    public int getEmployeeID() {
+        return employeeID;
+    }
+    @Override
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+    
+    
+    @Override
+    public char getGender() {
+        return gender;
+    }
+    @Override
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    
+    
+    @Override
+    public int getAge() {
+        return age;
+    }
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public double getEmployeePay() {
+        employeePay = (commissionPay + getAnnualSalary());
+        
+        return employeePay;
+    }
     
 }
